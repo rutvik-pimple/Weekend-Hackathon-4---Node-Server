c3df9f0f3e64a109bf4404ee241124834d3f5349
rutvik_pimple <rdp1621998@gmail.com>
2020-11-10 17:24:35 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+    const url = req.url
+    if(url === '/welcome'){
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.write('Welcome to Dominos!')
+        return res.end()
+    }else if(url === '/contact'){
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+            }))
+        return res.end()
+    }else{
+        res.writeHead(404)
+        return res.end()
+    }
   
 }
 
 module.exports = httpServer;
+
+httpServer.listen(8081)
